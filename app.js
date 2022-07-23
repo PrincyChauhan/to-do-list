d125bcc10dc33627a0655b5ec62a9c85f0a0b57b
PrincyChauhan <chauhanprincee7@gmail.com>
2022-07-23 19:12:09 +0530

    Mongodb added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 
 const date = require('./date');
 const app = express();
@@ -8,23 +10,52 @@ app.use(bodyParser.urlencoded({ extenend: true }))
 app.use(express.static("public"))
 app.set("view engine", "ejs");
 
-let items = ["Buy food", "Cook food", "Eat food"];
-let workItems = [];
+mongoose.connect("mongodb://localhost:27017/toDOListDB", { useNewUrlParser: true });
+const itemSchema = {
+    name: String
+}
+const Item = mongoose.model("Item", itemSchema)
+
+const item1 = new Item({
+    name: "Welcome to your todoList",
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new list",
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item.",
+});
+
+const defaultItems = [item1, item2, item3];
+
 
 app.get('/', (req, res) => {
-    let day = date.getDate()
-    res.render("list", { listTitle: day, newItemList: items })
+    Item.find({}, (err, foundItems) => {
+        if (foundItems.length == 0) {
+            Item.insertMany(defaultItems, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("done");
+                }
+            })
+            res.redirect("/");
+        } else {
+            res.render("list", { listTitle: "Today", newItemList: foundItems })
+        }
+    })
 })
 
 app.post('/', (req, res) => {
-    let item = req.body.newItem
-    if (req.body.list == "Work") {
-        workItems.push(item)
-        res.redirect('/work')
-    } else {
-        items.push(item)
-        res.redirect('/')
-    }
+    let itemName = req.body.newItem
+    const item =new Item({
+        name:itemName
+    })
+    item.save();
+    res.redirect("/")
+ 
 })
 
 app.get('/work', (req, res) => {
